d01e29850570269223622c7a952bf971566e0ac0
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-08-09 03:36:05 +0530

    Update calculate_Total_Amount.js

--- groceryTracker/calculate_Total_Amount.js
+++ groceryTracker/calculate_Total_Amount.js
@@ -1,7 +1,7 @@
 let grocery1;
 let grocery2;
 let grocery3;
-function add() {
+function calculateTotalAmount() {
     grocery1 = parseFloat(document.getElementById('grocery1').value);
     grocery2 = parseFloat(document.getElementById('grocery2').value);
     grocery3 = parseFloat(document.getElementById('grocery3').value);
