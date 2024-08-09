d2b0dda249ec2f5358e6d4092c3385615746bde1
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-08-09 21:54:18 +0530

    Update restaurant_menu.js

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak - $55', 'Pasta - $20', 'Burger - $13', 'Salmon - $29'];
+const dessertMenu = ['Cake - $7', 'Ice Cream - $12', 'Pudding - $18', 'Fruit Salad - $15'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
